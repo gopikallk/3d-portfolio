c954f93e5fe69419ad387a74e8379a4f6b17ba26
gopika <gopikalk7@gmail.com>
2025-08-03 13:44:25 +0530

    initial 3d portfoloio commit

---
+++ src/component/HeroModel/HeroLights.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function HeroLights() {
+    return (
+        <>
+          <spotLight
+           position={[2,5,6]}
+           intensity={100}
+          />
+        </>
+    )
+}
+
+export default HeroLights
